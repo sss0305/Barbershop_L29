ed1682acbe7e1d29c20f0866c748419db15ec690
sss0305 <exploremexicoguide@gmail.com>
2023-06-15 09:48:00 -0600

    copy paste from old bshop

--- app.rb
+++ app.rb
@@ -14,10 +14,31 @@ end
 class Barber < ActiveRecord::Base
  end
 
-
+before do
+	@barbers = Barber.all
+end
 
 
 get '/' do
-	@barbers = Barber.all
+	# @barbers = Barber.all
+	@barbers = Barber.order "created_at DESC"
 	 erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username =	params[:username]
+	@email =	params[:email]
+	@phone =	params[:phone]
+	@date =		params[:date]
+	@time =		params[:time]
+	@barber =	params[:barber]
+	@color =	params[:color]
+
+	
+	erb "Thanks, #{@username}. Barber #{@barber} will be waiting for you on #{@date}. "
+
 end
