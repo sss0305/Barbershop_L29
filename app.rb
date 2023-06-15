1b55bae133e201d29e3a2a0ed99d7141e53532ea
sss0305 <exploremexicoguide@gmail.com>
2023-06-15 09:07:15 -0600

    add index.erb

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ class Client < ActiveRecord::Base
 end
 
 class Barber < ActiveRecord::Base
-end
+ end
 
 
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	 erb :index
 end
